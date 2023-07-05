27db2142bdb9ff77fcc1b2ab1e9b34c5236e86d3
TakeshiYoshidomi <takeshiyoshidomi@hiroshima-u.ac.jp>
2023-07-05 02:15:26 +0000

    first

--- agile.go
+++ agile.go
@@ -5,12 +5,22 @@ type Board struct {
 }
 
 func (b *Board) put(x, y int, u string) {
-       if u == "o" {
-             b.tokens[x + 3*y] = 1
-       }
+	if u == "o" {
+		b.tokens[x+3*y] = 1
+	} else {
+		b.tokens[x+3*y] = 2
+	}
+
 }
 func (b *Board) get(x, y int) string {
-        if b.tokens[x + 3*y] == 1 {
-             reture "o"
-        }
+	if b.tokens[x+3*y] == 1 {
+		return "o"
+	}
+	if b.tokens[x+3*y] == 2 {
+		return "x"
+	}
+	return "other"
+}
+
+func main() {
 }
--- agile_test.go
+++ agile_test.go
@@ -1,14 +1,27 @@
 package main
 
-import("testing")
+import (
+	"testing"
+)
 
-func TestPutToken01(t *testing.T){
-    b := &Board{
-        tokens: []int{0,0,0,0,0,0,0,0,0}
-    }
-    b.put(1,1, "o")
+func TestPutToken01(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	b.put(1, 1, "o")
 
-    if b.get(1,1) != "o"{
-        t.Errorf("......")
-    }
+	if b.get(1, 1) != "o" {
+		t.Errorf("......")
+	}
+}
+
+func TestPutToken02(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	b.put(0, 0, "x")
+
+	if b.get(0, 0) != "x" {
+		t.Errorf("Error")
+	}
 }
