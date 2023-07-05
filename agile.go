15920f8fef468ea52fc7b74a8f3b650789140a5e
TakeshiYoshidomi <takeshiyoshidomi@hiroshima-u.ac.jp>
2023-07-05 02:37:13 +0000

    second

--- agile.go
+++ agile.go
@@ -5,10 +5,12 @@ type Board struct {
 }
 
 func (b *Board) put(x, y int, u string) {
-	if u == "o" {
+	if b.tokens[x+3*y] != 0 {
+		return
+	} else if u == "o" {
 		b.tokens[x+3*y] = 1
 	} else {
-		b.tokens[x+3*y] = 2
+		b.tokens[x+3*y] = -1
 	}
 
 }
@@ -16,8 +18,23 @@ func (b *Board) get(x, y int) string {
 	if b.tokens[x+3*y] == 1 {
 		return "o"
 	}
-	if b.tokens[x+3*y] == 2 {
+	if b.tokens[x+3*y] == -1 {
 		return "x"
+	}
+	return "other"
+}
+
+func (b *Board) judge() string {
+	i := 0
+	for i <= 2 {
+		sum := 0
+		sum = b.tokens[i*3] + b.tokens[i*3+1] + b.tokens[i*3+2]
+		if sum == 3 {
+			return "o win"
+		} else if sum == -3 {
+			return "x win"
+		}
+		i++
 	}
 	return "other"
 }
--- agile_test.go
+++ agile_test.go
@@ -25,3 +25,66 @@ func TestPutToken02(t *testing.T) {
 		t.Errorf("Error")
 	}
 }
+
+func TestPutToken03(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	b.put(0, 0, "x")
+	b.put(0, 0, "o")
+
+	if b.get(0, 0) != "x" {
+		t.Errorf("Error")
+	}
+}
+
+func TestPutToken04(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	b.put(0, 0, "o")
+	b.put(1, 0, "o")
+	b.put(2, 0, "o")
+
+	if b.judge() != "o win" {
+		t.Errorf("Error")
+	}
+}
+
+func TestPutToken05(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	b.put(0, 0, "x")
+	b.put(1, 0, "x")
+	b.put(2, 0, "x")
+
+	if b.judge() != "x win" {
+		t.Errorf("Error")
+	}
+}
+
+func TestPutToken06(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	b.put(3, 0, "x")
+	b.put(4, 0, "x")
+	b.put(5, 0, "x")
+
+	if b.judge() != "x win" {
+		t.Errorf("Error")
+	}
+}
+func TestPutToken07(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	b.put(0, 0, "x")
+	b.put(0, 1, "x")
+	b.put(0, 2, "x")
+
+	if b.judge() != "x win" {
+		t.Errorf("Error")
+	}
+}
